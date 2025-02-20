90bd602fd4b39bd685472ae6ffe1f0d3ffb8f585
Sardar Hashim Khan <105148784+sardarhashimkhan@users.noreply.github.com>
2025-02-21 02:16:58 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DaonFrameworks",
-            url: "https://github.com/aiondigital/daon-framework/releases/download/3.0/DaonFrameworks.zip",
+            url: "https://github.com/sardarhashimkhan/daon/releases/download/1.0/DaonFrameworks.zip",
             checksum: "ccd0a38db8e143cda18adcbb0a7c57007b7d79d23e72a2c178aa5d3441a0b9de", branch: "SPM/BBYN_SPM")
         )
     ]

7f776fc309963447e733a95e4b2c0725a22398e7
Sardar Hashim Khan <105148784+sardarhashimkhan@users.noreply.github.com>
2025-02-21 01:59:58 +0500

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "DaonFrameworks",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "DaonFrameworks",
+            targets: ["DaonFrameworks"]
+        ),
+    ],
+    dependencies: [
+        // Add any dependencies if required
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DaonFrameworks",
+            url: "https://github.com/aiondigital/daon-framework/releases/download/3.0/DaonFrameworks.zip",
+            checksum: "ccd0a38db8e143cda18adcbb0a7c57007b7d79d23e72a2c178aa5d3441a0b9de", branch: "SPM/BBYN_SPM")
+        )
+    ]
+)

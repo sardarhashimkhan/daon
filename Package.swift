c07ed82b2197fa9b1a39731a8673550d70ee4221
Hashim Khan <hashim@aiondigital.com>
2025-03-12 10:11:30 +0500

    no message

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DaonFrameworks",
-            path: "Sources/DaonFrameworks/DaonFrameworks.xcframework"
+            path: "DaonFrameworks/DaonFrameworks.xcframework"
         )
     ]
 )

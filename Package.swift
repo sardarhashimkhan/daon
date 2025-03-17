0ff0bcb7ea106e16146a566784fb20eefaeb03b1
Hashim Khan <hashim@aiondigital.com>
2025-03-18 04:44:53 +0500

    no message

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DaonFrameworks",
-            targets: ["DaonFrameworks"]),
+            targets: ["DaonDocument"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.

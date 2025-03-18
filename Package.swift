71260748968bf2945f878b9115992c1cf9008729
Hashim Khan <hashim@aiondigital.com>
2025-03-18 06:35:28 +0500

    no message

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DaonFrameworks",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DaonFrameworks",
-            targets: ["DaonDocument"]),
+            targets: ["DaonFrameworks"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "DaonDocument",
             path: "DaonFrameworks/DaonDocument.xcframework"
@@ -22,10 +22,5 @@ let package = Package(
             name: "DaonFrameworks",
             dependencies: ["DaonDocument"]
         ),
-        .testTarget(
-            name: "DaonFrameworksTests",
-            dependencies: ["DaonFrameworks"]
-        ),
     ]
 )
-
